b4a7bc8b5a22ca7956417a33f77c6a79cb6526ed
Baot Lake <BoatLake@gmail.com>
2020-07-19 09:44:13 +0800

    build

---
+++ build/precache-manifest.3c75c91ddf8898a15e7dd0bf7c2651ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70b78132b6543fa73503406a369b749",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b9173d0ea109a33e0b",
+    "url": "/static/css/main.dff603da.chunk.css"
+  },
+  {
+    "revision": "c1d9d0a644e1b578a962",
+    "url": "/static/js/2.d3f1630e.chunk.js"
+  },
+  {
+    "revision": "0932d77462e71f5b93a665a469fd7f93",
+    "url": "/static/js/2.d3f1630e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b9173d0ea109a33e0b",
+    "url": "/static/js/main.d3aa5faa.chunk.js"
+  },
+  {
+    "revision": "7a11aaa6b03ae2f95565",
+    "url": "/static/js/runtime-main.9002d5ac.js"
+  },
+  {
+    "revision": "4030fa089446d204ea6185284fdcdb44",
+    "url": "/static/media/audio.4030fa08.svg"
+  },
+  {
+    "revision": "cd1bf69814ce5eca698f7c58c3e89cfd",
+    "url": "/static/media/explainBoxArrow.cd1bf698.svg"
+  },
+  {
+    "revision": "26f6d80ddce4307b0de9d34f2b80939a",
+    "url": "/static/media/more.26f6d80d.svg"
+  },
+  {
+    "revision": "4645967cd7a4e63fc2e9de90ccee71a6",
+    "url": "/static/media/open.4645967c.svg"
+  },
+  {
+    "revision": "e07fbbc978f57db9be1cc859aaa6bd4d",
+    "url": "/static/media/star.e07fbbc9.svg"
+  }
+]);

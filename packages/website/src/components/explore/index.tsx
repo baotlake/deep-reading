8c8fef3292b982a3bc88fcae23916038ccd087f2
Baotlake <baotlake@gmail.com>
2021-03-28 15:47:51 +0800

    delete unused vars Please enter the commit message for your changes. Lines starting

--- packages/website/src/components/explore/index.tsx
+++ packages/website/src/components/explore/index.tsx
@@ -1,60 +1,65 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react'
 
-import './explore.scss';
-import { withRouter } from 'react-router';
+import './explore.scss'
 
-import { ItemCard } from '../../containers/Home';
+import { ItemCard } from '../../containers/Home'
 
 import { exploreData } from '../../assets/explore'
 
 import NavigationBar from './NavigationBar'
 
-
 function ExploreApp() {
-
     const navigationList = [
         {
             title: '推荐',
             key: 'recommended',
             color: '',
-        }, {
+        },
+        {
             title: '入门级',
             key: 'gettingStarted',
             color: '',
-        }, {
+        },
+        {
             title: '英语教材',
             key: 'textbook',
             color: '',
-        }, {
+        },
+        {
             title: '名著小说',
             key: 'classicNovel',
             color: '',
-        }, {
+        },
+        {
             title: '技术文档',
             key: 'technicalDocuments',
             color: '',
-        }, {
+        },
+        {
             title: '时事新闻',
             key: 'news',
             color: '',
-        }, {
+        },
+        {
             title: '政治金融',
             key: 'politicalFinance',
             color: '',
-        }, {
+        },
+        {
             title: '文史哲',
             key: 'history',
             color: '',
-        }, {
+        },
+        {
             title: '体育运动',
             key: 'sports',
             color: '',
-        }
+        },
     ]
 
     const navigationData = exploreData as { [index: string]: { list: any[] } }
 
-    const [navigationIndex, setIndex] = useState(0);
+    const [navigationIndex, setIndex] = useState(0)
 
     const seletedKey = navigationList[navigationIndex].key
 
@@ -69,14 +74,12 @@ function ExploreApp() {
             </header>
 
             <div className="wrp-page wrp-card-container">
-                {
-                    navigationData[seletedKey].list.map((item, index) => (
-                        <ItemCard data={item} key={item.url}></ItemCard>
-                    ))
-                }
+                {navigationData[seletedKey].list.map((item, index) => (
+                    <ItemCard data={item} key={item.url}></ItemCard>
+                ))}
             </div>
         </>
     )
 }
 
-export default ExploreApp;
+export default ExploreApp
